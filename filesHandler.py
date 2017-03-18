3ee18687d9aea26c2eac2f421215470423759a9e
Neal DeBuhr <ndebuhr@protonmail.com>
2017-03-18 13:49:54 -0400

    Add equations csv generation to filesHandler.py

--- filesHandler.py
+++ filesHandler.py
@@ -1,12 +1,37 @@
 import csv
 import argparse
- 
-parser.add_argument('-i','--input', help='Input file name',required=True)
- 
-print ("Input file: %s" % args.input )
+import itertools
 
-import csv
-with open(args.input, 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print ', '.join(row)
+paramsTherm=['h','T','P','s']
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--input',required=True)
+parser.add_argument('-o','--output',required=True)
+args = parser.parse_args()
+
+print("Input file: %s" % args.input)
+
+nestedPts=[]
+with open(args.input, 'rt') as csvfile:
+    filePts = csv.reader(csvfile, delimiter=',', quotechar="'")
+    for row in filePts:
+        nestedPts.append(','.join(row))
+
+lenPts=len(nestedPts[0].split(','))-1
+print("Number of points: %s" % lenPts)
+
+outRows=[['','Node']+paramsTherm+['']+paramsTherm+['Node']]
+for rowInd in range(0,lenPts):
+    outRow=['Eqn ' + str(rowInd+1)]
+    for paramInd in range(0,len(paramsTherm)+1):
+        outRow.append('')
+    outRow.append('=')
+    for paramInd in range(0,len(paramsTherm)+1):
+        outRow.append('')
+    outRows.append(outRow)
+
+with open(args.output, 'wt') as csvfile:
+    fileEqns = csv.writer(csvfile, delimiter=',',
+                            quotechar="'", quoting=csv.QUOTE_MINIMAL)
+    for row in outRows:
+        fileEqns.writerow(row)
