b23ed2d6d74c4604e9bb7b55faf121661ee9f785
Neal DeBuhr <ndebuhr@protonmail.com>
2017-03-28 17:34:36 -0400

    Develop program for points file generation

--- statePointsGen.py
+++ statePointsGen.py
@@ -5,3 +5,28 @@
 import csv
 import argparse
 import itertools
+import string
+
+numPoints=int(input('Number of points in analysis:'))
+num2alpha = dict(zip(range(1, 27), string.ascii_uppercase))
+outRow=['']
+outRows=[]
+paramsPoints=['mdot','h','T','P','s']
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o','--output',required=True)
+args = parser.parse_args()
+
+for point in range(1,numPoints+1):
+    outRow.append(num2alpha[point])
+outRows.append(outRow)
+for param in paramsPoints:
+    outRows.append([param])
+
+with open(args.output, 'wt') as csvfile:
+    fileEqns = csv.writer(csvfile, delimiter=',',
+                            quotechar="'", quoting=csv.QUOTE_MINIMAL)
+    for row in outRows:
+        fileEqns.writerow(row)
+
+print("Output file: %s" % args.output)
