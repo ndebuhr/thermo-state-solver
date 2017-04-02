2e63a1ae4cd729fa6bcdb915005432321d856033
Neal DeBuhr <ndebuhr@protonmail.com>
2017-04-02 16:57:37 -0400

    Match PEP 8 standard for string quotes, whitespace, function names, and script names

--- state_eqns_gen.py
+++ state_eqns_gen.py
@@ -6,38 +6,38 @@ import csv
 import argparse
 import itertools
 
-def csvRowWriter(outFile,inList):
+def csv_row_writer(outFile,inList):
     with open(args.output, 'wt') as csvfile:
         writeCsv = csv.writer(csvfile, delimiter=',',
                               quotechar="'", quoting=csv.QUOTE_MINIMAL)
         for row in inList:
             writeCsv.writerow(row)
 
-def readCsvRows(inFile):
-    result=[]
+def read_csv_rows(inFile):
+    result = []
     with open(inFile, 'rt') as csvfile:
         rows = csv.reader(csvfile, delimiter=',', quotechar="'")
         for row in rows:
             result.append(','.join(row))
     return result
 
-paramsTherm=['h','T','P','s']
+paramsTherm = ['h','T','P','s']
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i','--input',required=True)
 parser.add_argument('-o','--output',required=True)
 args = parser.parse_args()
 
-print("Input file: %s" % args.input)
+print('Input file: %s' % args.input)
 
-nestedPts=readCsvRows(args.input)
+nestedPts = read_csv_rows(args.input)
 
-lenPts=len(nestedPts[0].split(','))-1
-print("Number of points: %s" % lenPts)
+lenPts = len(nestedPts[0].split(','))-1
+print('Number of points: %s' % lenPts)
 
-outRows=[['','(Node)']+paramsTherm+['']+paramsTherm+['(Node)','','Q','W']]
+outRows = [['','(Node)']+paramsTherm+['']+paramsTherm+['(Node)','','Q','W']]
 for rowInd in range(0,lenPts):
-    outRow=['Eqn ' + str(rowInd+1)]
+    outRow = ['Eqn ' + str(rowInd+1)]
     for paramInd in range(0,len(paramsTherm)+1):
         outRow.append('')
     outRow.append('-')
@@ -46,13 +46,13 @@ for rowInd in range(0,lenPts):
     outRow.append('=')
     outRows.append(outRow)
 outRows.append([''])
-outRow=['']
+outRow = ['']
 for eqnInd in range(0,lenPts):
     outRow.append('E'+str(eqnInd+1))
 outRows.append(outRow)
-outRow=['Balance']
+outRow = ['Balance']
 outRows.append(outRow)
 
-csvRowWriter(args.output,outRows)
+csv_row_writer(args.output,outRows)
 
-print("Output file: %s" % args.output)
+print('Output file: %s' % args.output)
--- state_points_gen.py
+++ state_points_gen.py
@@ -1,5 +1,5 @@
 # Thermo State Solver
-# Solves for state parameters at various points in a simple thermodynamic model 
+# Solves for state parameters at various points in a simple thermodynamic model
 # Developed by Neal DeBuhr
 
 import csv
@@ -7,21 +7,21 @@ import argparse
 import itertools
 import math
 
-def csvRowWriter(outFile,inList):
+def csv_row_writer(outFile,inList):
     with open(args.output, 'wt') as csvfile:
         writeCsv = csv.writer(csvfile, delimiter=',',
                               quotechar="'", quoting=csv.QUOTE_MINIMAL)
         for row in inList:
             writeCsv.writerow(row)
 
-def letterIncrementer(size):
-    result=[]
-    letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    letList=list(letters)
+def letter_incrementer(size):
+    result = []
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    letList = list(letters)
     for point in range(0,size):
-        if point/26>=1:
-            let1=math.floor(point/26)-1
-            let2=point%26
+        if point/26 >= 1:
+            let1 = math.floor(point/26)-1
+            let2 = point%26
             result.append(letList[let1]+letList[let2])
         else:
             result.append(letList[point])
@@ -31,15 +31,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-o','--output',required=True)
 args = parser.parse_args()
 
-paramsPoints=['mdot','h','T','P','s']
-outRows=[]
+paramsPoints = ['mdot','h','T','P','s']
+outRows = []
 
-numPoints=int(input('Number of points in analysis:'))
+numPoints = int(input('Number of points in analysis:'))
 
-outRows.append(['']+letterIncrementer(numPoints))
+outRows.append(['']+letter_incrementer(numPoints))
 for param in paramsPoints:
     outRows.append([param])
 
-csvRowWriter(args.output,outRows)
+csv_row_writer(args.output,outRows)
 
-print("Output file: %s" % args.output)
+print('Output file: %s' % args.output)
