55a0797aa70ef91ce1ad9d29f313fa58a1db761d
Neal DeBuhr <ndebuhr@protonmail.com>
2017-05-01 06:18:57 -0400

    Create file to read in parameter vectors from user forms

---
+++ read_forms.py
@@ -0,0 +1,41 @@
+import csv
+import argparse
+import itertools
+
+from thermo_utils import csv_row_writer, read_csv_rows
+
+# Read input/output arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-ip','--inputpoints',required=True)
+parser.add_argument('-ie','--inputeqns',required=True)
+parser.add_argument('-v','--version',required=False)
+args = parser.parse_args()
+print('Input points file: %s' % args.inputpoints)
+print('Input equations file: %s' % args.inputeqns)
+
+# Read in points file
+filePts = read_csv_rows(args.inputpoints)
+
+# Read in equations file
+fileEqns = read_csv_rows(args.inputeqns)
+
+# Create property vectors from points file
+for ptsRow in range(0,len(filePts)):
+    filePts[ptsRow]=filePts[ptsRow].split(",")
+    if ptsRow>0:
+        if filePts[ptsRow][0]=='mdot':
+            mdot_v=filePts[ptsRow]
+        elif filePts[ptsRow][0]=='h':
+            h_v=filePts[ptsRow]
+        elif filePts[ptsRow][0]=='T':
+            T_v=filePts[ptsRow]
+        elif filePts[ptsRow][0]=='P':
+            P_v=filePts[ptsRow]
+        elif filePts[ptsRow][0]=='s':
+            s_v=filePts[ptsRow]
+
+print(mdot_v)
+print(h_v)
+print(T_v)
+print(P_v)
+print(s_v)
