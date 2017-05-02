05466475014d627cef4d5b92bdd743fb17b822da
Neal DeBuhr <ndebuhr@protonmail.com>
2017-05-02 06:10:32 -0400

    Pull full system energy balance into separate form and generation function

--- balance_gen.py
+++ balance_gen.py
@@ -4,9 +4,6 @@ import itertools
 
 from thermo_utils import csv_row_writer, read_csv_rows
 
-# Define list of intrinsic thermodynamic properties
-paramsTherm = ['h','T','P','s']
-
 # Read input/output arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-i','--input',required=True)
@@ -22,22 +19,14 @@ nestedPts = read_csv_rows(args.input)
 lenPts = len(nestedPts[0].split(','))-1
 print('Number of points: %s' % lenPts)
 
-# Generate equation rows, with each row being an energy balance across a
-# component
-outRows = [['','(Node)']
-           +paramsTherm
-           +['']
-           +paramsTherm
-           +['(Node)','','Q','W']] # Column headers
-for rowInd in range(0,lenPts):
-    outRow = ['Eqn ' + str(rowInd+1)]
-    for paramInd in range(0,len(paramsTherm)+1):
-        outRow.append('')
-    outRow.append('-')
-    for paramInd in range(0,len(paramsTherm)+1):
-        outRow.append('')
-    outRow.append('=')
-    outRows.append(outRow)
+outRows=[]
+# Generate table for full system energy balance
+outRow = ['']
+for eqnInd in range(0,lenPts):
+    outRow.append('E'+str(eqnInd+1))
+outRows.append(outRow) # Full system column headers
+outRow = ['Balance']
+outRows.append(outRow) # Full system row header
 
 # Write all rows to equations CSV file
 csv_row_writer(args.output,outRows)
