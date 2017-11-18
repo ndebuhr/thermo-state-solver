aa362a01aa074bc728396beeea4610ad177c07b5
ndebuhr <ndebuhr@protonmail.com>
2017-11-18 14:38:26 -0500

    Readability and maintainability improvements

--- state_eqns_gen.py
+++ state_eqns_gen.py
@@ -1,44 +1,45 @@
-import csv
+""" Generates form for defining the energy balance across individual
+elements in the thermodynamic model
+"""
 import argparse
-import itertools
 
 from thermo_utils import csv_row_writer, read_csv_rows
 
 # Define list of intrinsic thermodynamic properties
-paramsTherm = ['h','T','P','s']
+PARAMS_THERM = ['h', 'T', 'P', 's']
 
 # Read input/output arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-i','--input',required=True)
-parser.add_argument('-o','--output',required=True)
-parser.add_argument('-v','--version',required=False)
+parser.add_argument('-i', '--input', required=True)
+parser.add_argument('-o', '--output', required=True)
+parser.add_argument('-v', '--version', required=False)
 args = parser.parse_args()
 print('Input file: %s' % args.input)
 
 # Read in data from points CSV file
-nestedPts = read_csv_rows(args.input)
+NESTED_PTS = read_csv_rows(args.input)
 
 # Determine number of points in the points CSV file using first row length
-lenPts = len(nestedPts[0].split(','))-1
-print('Number of points: %s' % lenPts)
+LEN_PTS = len(NESTED_PTS[0].split(','))-1
+print('Number of points: %s' % LEN_PTS)
 
 # Generate equation rows, with each row being an energy balance across a
 # component
-outRows = [['','(Node)']
-           +paramsTherm
-           +['']
-           +paramsTherm
-           +['(Node)','','Q','W']] # Column headers
-for rowInd in range(0,lenPts):
+OUT_ROWS = [['', '(Node)']
+            +PARAMS_THERM
+            +['']
+            +PARAMS_THERM
+            +['(Node)', '', 'Q', 'W']] # Column headers
+for rowInd in range(0, LEN_PTS):
     outRow = ['Eqn ' + str(rowInd+1)]
-    for paramInd in range(0,len(paramsTherm)+1):
+    for paramInd in range(0, len(PARAMS_THERM)+1):
         outRow.append('')
     outRow.append('-')
-    for paramInd in range(0,len(paramsTherm)+1):
+    for paramInd in range(0, len(PARAMS_THERM)+1):
         outRow.append('')
     outRow.append('=')
-    outRows.append(outRow)
+    OUT_ROWS.append(outRow)
 
 # Write all rows to equations CSV file
-csv_row_writer(args.output,outRows)
+csv_row_writer(args.output, OUT_ROWS)
 print('Output file: %s' % args.output)
