9e1a0f9ac4caabf386c1e3103ec69f269131a6b9
ndebuhr <ndebuhr@protonmail.com>
2017-11-18 19:43:57 -0500

    Readability and maintainability improvements

--- state_points_gen.py
+++ state_points_gen.py
@@ -1,27 +1,27 @@
-import csv
+""" Generates form to capture the thermodynamic state variables
+at all established points in the system
+"""
 import argparse
-import itertools
-import math
 
 from thermo_utils import csv_row_writer, letter_incrementer
 
 # Read input/output arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-o','--output',required=True)
-parser.add_argument('-p','--points',required=True)
+parser.add_argument('-o', '--output', required=True)
+parser.add_argument('-p', '--points', required=True)
 args = parser.parse_args()
-numPoints=int(args.points)
+NUM_POINTS = int(args.points)
 
 # Define mdot and intrinsic thermodynamic properties
-paramsPoints = ['mdot','h','T','P','s']
+PARAMS_POINTS = ['mdot', 'h', 'T', 'P', 's']
 
 # Generate list/table with point labels as the column headers and intrinsic
-# thermodyanmic properties (and mass flow) as the row headers 
-outRows = []
-outRows.append(['']+letter_incrementer(numPoints)) # Column headers
-for param in paramsPoints:
-    outRows.append([param]) # Additional rows
+# thermodyanmic properties (and mass flow) as the row headers
+OUT_ROWS = []
+OUT_ROWS.append(['']+letter_incrementer(NUM_POINTS)) # Column headers
+for param in PARAMS_POINTS:
+    OUT_ROWS.append([param]) # Additional rows
 
 # Write rows to points file
-csv_row_writer(args.output,outRows)
+csv_row_writer(args.output, OUT_ROWS)
 print('Output file: %s' % args.output)
